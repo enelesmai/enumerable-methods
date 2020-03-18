b773a60cb3adb9ab094409552fc4af44d7da32da
enelesmai <xochitl.selene.fp@gmail.com>
2020-03-17 23:46:57 -0600

    using single quotes

--- main.rb
+++ main.rb
@@ -1,37 +1,37 @@
-require "./my_enumerable_module"
-require "./my_test"
+require './my_enumerable_module'
+require './my_test'
 
-puts "select an option to test enumerable"
-puts "1) my_each"
-puts "2) my_each_with_index"
-puts "3) my_select"
-puts "4) my_all?"
-puts "5) my_any?"
-puts "6) my_none?"
-puts "7) my_count"
-puts "8) my_map"
-puts "9) my_inject"
+puts 'select an option to test enumerable'
+puts '1) my_each'
+puts '2) my_each_with_index'
+puts '3) my_select'
+puts '4) my_all?'
+puts '5) my_any?'
+puts '6) my_none?'
+puts '7) my_count'
+puts '8) my_map'
+puts '9) my_inject'
 
 option_enumerable = gets.chomp
 
 case option_enumerable
-when "1"
+when '1'
     test_my_each
-when "2"
+when '2'
     test_my_each_with_index
-when "3"
+when '3'
     test_my_select
-when "4"
+when '4'
     test_my_all
-when "5"
+when '5'
     test_my_any
-when "6"
+when '6'
     test_my_none
-when "7"
+when '7'
     test_my_count
-when "8"
+when '8'
     test_my_map
-when "9"
+when '9'
     test_my_inject
 else
     "That method doesn't exists"

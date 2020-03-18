28c667015edc30afa9d12b1b4b5adc0b6f7a1c2e
enelesmai <xochitl.selene.fp@gmail.com>
2020-03-17 23:34:59 -0600

    deleted code not related

--- main.rb
+++ main.rb
@@ -14,26 +14,6 @@ puts "9) my_inject"
 
 option_enumerable = gets.chomp
 
-case option_enumerable 
-      
-    # here 'when' statement contains 
-    # the two values 
-    when "1","2"
-        puts "You order Espresso!"
-      
-    when "3","4"
-        puts "You order Short Macchiato!"
-      
-    when "5","6"
-        puts "You order Ristretto!"
-      
-    when "7","8"
-        puts "You order Cappuccino!"
-      
-else
-    "No Order!"
-end
-
 case option_enumerable
 when "1"
     test_my_each

c2cdc112d337ee3b8d15d869fe69ecdbd18d320a
enelesmai <xochitl.selene.fp@gmail.com>
2020-03-19 14:13:35 -0600

    added empty line after guard clause

--- my_enumerable_module.rb
+++ my_enumerable_module.rb
@@ -1,6 +1,7 @@
 module Enumerable
   def my_each
     return to_enum(:my_each) unless block_given?
+
     for i in 0...length
       yield self[i]
     end
@@ -8,6 +9,7 @@ module Enumerable
 
   def my_each_with_index(start_index = 0)
     return to_enum(:my_each_with_index) unless block_given?
+    
     for idx in 0...length
       yield(self[idx], idx + start_index)
     end
@@ -16,6 +18,7 @@ module Enumerable
 
   def my_select
     return to_enum(:my_select) unless block_given?
+    
     result = []
     my_each do |item|
       result.push(item) if yield(item)
@@ -25,9 +28,11 @@ module Enumerable
 
   def my_all?(pattern = nil)
     return false if pattern.class == Regexp
+    
     result = true
     if block_given?
       return to_enum(:my_all) unless block_given?
+    
       my_each do |item|
         result &&= yield(item)
       end
@@ -45,9 +50,11 @@ module Enumerable
 
   def my_any?(pattern = nil)
     return false if pattern.class == Regexp
+    
     result = true
     if block_given?
       return to_enum(:my_any) unless block_given?
+    
       my_each do |item|
         result ||= yield(item)
       end
@@ -65,9 +72,11 @@ module Enumerable
 
   def my_none?(pattern = nil)
     return true if pattern.class == Regexp
+    
     result = false
     if block_given?
       return to_enum(:my_any) unless block_given?
+    
       my_each do |item|
         result ||= yield(item)
       end
@@ -87,6 +96,7 @@ module Enumerable
     counter = 0   
       if(item.nil?)
         return length unless block_given?
+    
         my_each do |item|
           counter += 1 if yield item
         end
@@ -100,6 +110,7 @@ module Enumerable
 
   def my_map(procedure = nil)
     return to_enum(:my_map) unless block_given?
+    
     result = []
     if procedure.nil?
       my_each do |item|

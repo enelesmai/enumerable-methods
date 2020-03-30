f5e14272df9864576f6e9ea75ebe841e95bcfebe
rubenpazch <rubenpazchuspe@outlook.com>
2020-03-30 16:21:55 -0500

    adding context for test cases on my each with index

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -5,13 +5,41 @@ describe Enumerable do
   let(:array) { [1, 2, 3, 4, 5] }
   describe '#my_each' do
     it 'returns enum when no block is given' do
-      expect(array.my_each).to be_an Enumerator
+      eitempect(array.my_each).to be_an Enumerator
     end
     it 'returns self when block is given' do
-      expect(array.my_each { |x| puts x }).to eql(array.each { |x| puts x })
+      expected = array.each { |item| puts item }
+      eitempect(array.my_each { |item| puts item }).to eql(expected)
     end
-    it 'loops to each element to do the given operation' do
-      expect(array.my_each { |x| x += 1 }).to eql(array.each { |x| x += 1 })
+  end
+
+  describe '#my_each_with_index' do
+    context 'when no block is given' do
+      it 'returns enum ' do
+        eitempect(array.my_each_with_index).to be_an Enumerator
+      end
+    end
+    context 'when block is given' do
+      it 'returns self' do
+        expected = array.each_with_index { |item, _index| puts item }
+        eitempect(array.my_each_with_index { |item, _index| puts item }).to eql(expected)
+      end
+    end
+    context 'when operation is given for item' do
+      it 'loops to each item' do
+        expected = array.each_with_index { |item, _index| item + 1 }
+        eitempect(array.my_each_with_index { |item, _index| item + 1 }).to eql(expected)
+      end
+    end
+    context 'when operation is given for index' do
+      it 'loops to each index ' do
+        expected = array.each_with_index { |_item, index| index + 1 }
+        eitempect(array.my_each_with_index { |_item, index| index + 1 }).to eql(expected)
+      end
+      it 'loops to each index' do
+        expected = array.each_with_index { |_item, index| index * 1 }
+        eitempect(array.my_each_with_index { |_item, index| index * 1 }).to eql(expected)
+      end
     end
   end
 end
